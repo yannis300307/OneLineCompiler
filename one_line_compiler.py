9eba48ee34de7ff8b9fca5a9a4e3b43910bfce9d
yannis300307 <52321382+yannis300307@users.noreply.github.com>
2024-03-21 21:07:51 +0100

    Update implémentation for et if

--- one_line_compiler.py
+++ one_line_compiler.py
@@ -1,44 +1,101 @@
 with open("one_line_in.txt", "r") as file:
-    data = file.read()
+    data_ = file.read()
 
-init_lines = []
-loop_lines = []
-imports = []
 
-lines = data.split("\n")
-
-line_count = 1
-lines_cat = 0
-for i in lines:
-    if i == "":
-        continue
-    if i.startswith("#Init"):
-        lines_cat = 1
-    elif i.startswith("#Loop"):
-        lines_cat = 2
+def eval_block(data):
+    out = []
+    if isinstance(data, str):
+        lines = data.split("\n")
     else:
-        if lines_cat == 1:
-            if i.startswith("import"):
-                imports.extend(i.split()[1:])
-            else:
-                if i.count("=") == 1 and compile(i, "", "exec"):
-                    elems = i.split("=")
-                    init_lines.append(elems[0] + ":=" + elems[1])
+        lines = data
+    i = 0
+    while i != len(lines):
+        j = lines[i]
+        if j == "" or j.startswith("#"):
+            i += 1
+            continue
+        if j.startswith("exit"):
+            out.append("__exit__:=True")
+        elif j.startswith("import"):
+            libs = j.split()[1:]
+            for lib in libs:
+                out.append(f"{lib}:=__import__(\"{lib}\")")
+        elif j.count("=") == 1 and compile(j, "", "exec"):
+            operands = j.split("=")
+            out.append(operands[0] + ":=" + operands[1])
+        elif j.startswith("for"):
+            block_lines = []
+            indent_len = 0
+            for_line = j
+            for k in lines[i+1]:
+                if k == " ":
+                    indent_len += 1
                 else:
-                    init_lines.append(i)
-        elif lines_cat == 2:
-            if i.count("=") == 1 and compile(i, "", "exec"):
-                elems = i.split("=")
-                loop_lines.append(elems[0] + ":=" + elems[1])
-            else:
-                loop_lines.append(i)
+                    break
+            i += 1
+            indent = indent_len
+            while indent >= indent_len and i < len(lines):
+                indent = 0
+                for k in lines[i]:
+                    if k == " ":
+                        indent += 1
+                    else:
+                        break
+                if indent >= indent_len:
+                    block_lines.append(lines[i][indent_len:])
+                i += 1
+            block = eval_block(block_lines)
+            out.append("_:=[(" + ", ".join(block) + ") " + for_line + "]")
+            i -= 1
+        elif j.startswith("if"):
+            block_lines = []
+            indent_len = 0
+            if_line = j
+            for k in lines[i + 1]:
+                if k == " ":
+                    indent_len += 1
+                else:
+                    break
+            i += 1
+            indent = indent_len
+            while indent >= indent_len and i < len(lines):
+                indent = 0
+                for k in lines[i]:
+                    if k == " ":
+                        indent += 1
+                    else:
+                        break
+                if indent >= indent_len:
+                    block_lines.append(lines[i][indent_len:])
+                i += 1
+            block = eval_block(block_lines)
+            out.append("_:=((" + ", ".join(block) + ") " + if_line + " else None)")
+            i -= 1
         else:
-            raise SyntaxError(f"Erreur de syntaxe à la ligne : {line_count}.")
-    line_count += 1
+            out.append(j)
+        i += 1
+    return out
 
-init_lines = [f"{j}:=__import__(\"{j}\")" for j in imports] + init_lines
 
-print(init_lines)
-result = f"while ('__exit__' not in globals()): _=(({','.join(init_lines)}, __inited__:=True) if (not '__inited__' in globals()) else None, ({','.join(loop_lines)}))"
+init_start = 0
+loop_start = 0
+found_init = False
+found_loop = False
+lines = data_.split("\n")
+
+for i in enumerate(lines):
+    if i[1].startswith("#Init"):
+        init_start = i[0]
+        found_init = True
+    elif i[1].startswith("#Loop"):
+        loop_start = i[0]
+        found_loop = True
+if not found_loop:
+    loop_start = len(lines)-1
+
+init_lines = eval_block(lines[init_start:loop_start+1])
+loop_lines = eval_block(lines[loop_start:])
+
+result = f"while ('__exit__' not in globals()): _=((" + (f"_:=({','.join(init_lines)}, __inited__:=True) if (not '__inited__' in globals()) else None," if found_init else "") + (f"({','.join(loop_lines)})))" if found_loop else "))")
 
 print(result)
